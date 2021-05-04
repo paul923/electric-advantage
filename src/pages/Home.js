8374c8dd914a077d72095699c55f8c2d011d8b1a
Alex <hiennalex@gmail.com>
2021-05-03 18:51:08 -0700

    Update Home.js
    basic sign up modal (not all fields created yet)
    npm install react-bootstrap
    npm install bootstrap
    npm install jquery
    npm install popper.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./css/Home.css";
 import SignUpModal from "./pageComponents/SignUpModal";
 
