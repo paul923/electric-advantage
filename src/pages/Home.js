f2ddbc4c9ce86a5271c7efeeffa2f19a7321ca5d
Alex <hiennalex@gmail.com>
2021-05-03 18:42:47 -0700

    added basic login
    added basic login modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import "./css/Home.css";
+import SignUpModal from "./pageComponents/SignUpModal";
 
 export default function Home() {
-  const [testText, setTestText] = React.useState("Home");
+  const [registrationState, setRegistrationState] = useState(false);
+  const [modalState, setModalState] = useState(false);
+
   return (
     <div>
-      <h2>{testText}</h2>
+      <Button className="btn-success" onClick={() => setModalState(true)}>
+        Sign Up
+      </Button>
+      <SignUpModal
+        modalState={modalState}
+        setRegistrationState={setRegistrationState}
+        registrationState={registrationState}
+        setModalState={setModalState}
+      />
     </div>
   );
 }
