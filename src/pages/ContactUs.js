4e311cef79957d04cc876db7535ec4fa54504fed
jennieyang95 <jennieyang95@gmail.com>
2021-05-05 12:44:08 -0700

    comment box added
    will need to re-format to look nicer

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,15 +1,31 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/Home.css";
+import "./css/ContactUs.css";
+import { Button } from "react-bootstrap";
+
 import background from "../images/background.jpg";
 
 const ContactUs = () => {
     return (
-        
+
         <div>
-            <img src={background} alt="background" className="background"/>
+            <img src={background} alt="background" className="background" />
             <h1>Contact Us</h1>
+
+            <textarea placeholder='Send us a message'></textarea>
+            <Button className="btn-send"
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                Send
+        </Button>
         </div>
+
+
+
+
     )
 }
 
