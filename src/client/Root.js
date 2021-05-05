16d96fdac98776148f2ae4d0ac61278ec08f1c51
jaywalkingstreetparking <jjyyrim@gmail.com>
2021-05-05 01:23:16 -0700

    auth implemnetation

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../shared/App";
+import { AuthProvider } from "../components/AuthContext"
+
 
 const Root = () => (
   <BrowserRouter>
+    <AuthProvider>
+
     <App />
+    </AuthProvider>
+
   </BrowserRouter>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,7 @@ export { default as PostsPage } from "./PostsPage";
 export { default as Post } from "./Post";
 export { default as TestingPage } from "./TestingPage";
 export { default as ForbiddenPage } from "./ForbiddenPage";
+export { default as Signup } from "./Signup";
+export { default as Login } from "./Login";
+export { default as ForgotPassword } from "./ForgotPassword";
+export { default as Updateprofile } from "./UpdateProfile";
