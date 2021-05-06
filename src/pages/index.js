35f4aaec58f51509782b989340f5d93902c2a43e
seungyoon95 <seungyoon95@hotmail.com>
2021-05-06 09:27:49 -0700

    added search page to fix routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ export { default as SignIn} from "./SignIn";
 export { default as WhoWeAre } from "./WhoWeAre";
 export { default as ContactUs } from "./ContactUs";
 export { default as Profile } from "./Profile";
+export { default as Search } from "./Search";
 export { default as SearchResult } from "./SearchResult";
 export { default as SearchDetail} from "./SearchDetail";
