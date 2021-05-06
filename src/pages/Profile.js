17d44e1e345220476de898b4b3ffcf86a7eff5c0
jennieyang95 <jennieyang95@gmail.com>
2021-05-06 16:16:43 -0700

    textbox, change email/pw added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,14 +2,42 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/Home.css";
 import background from "../images/background.jpg";
+import { Button, Form, Carousel } from "react-bootstrap";
 
 const Profile = () => {
     return (
-        
-        <div>
-            <img src={background} alt="background" className="background"/>
-            <h1>Profile</h1>
-        </div>
+
+
+        <body className="body">
+            <div className="TopImage">
+                <img className="d-block w-100" src={background} alt="background" />
+            </div>
+            <div>
+                <div className="content">
+                    <h2 className="registrationText">Profile</h2>
+
+                    <div className="leftColumn">
+                        <Form>
+                            <Form.Group controlId="Firstname">
+                                <Form.Control type="email" placeholder="First Name" />
+                                <Form.Text />
+                            </Form.Group>
+                            <Form.Group controlId="LastName" className="leftColumnChildren">
+                                <Form.Control type="email" placeholder="Last Name" />
+                                <Form.Text />
+                            </Form.Group>
+                        </Form>
+                        <Button className="btn-success leftColumnChildren">Change Email/Password</Button>
+                    </div>
+                </div>
+                </div>
+
+       
+        </body>
+
+
+
+
     )
 }
 
