4efc864c2de426cc08084e1f47d1eb91cf1489b8
seungyoon95 <seungyoon95@hotmail.com>
2021-05-05 19:39:09 -0700

    empty SearchDetail page added for routing purpose

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home, WhoWeAre, ContactUs, SignIn, SearchResult } from "../pages";
+import { Home, WhoWeAre, ContactUs, SignIn, SearchResult, SearchDetail } from "../pages";
 import Menu from "../components/Menu";
 
 export default class App extends Component {
@@ -15,6 +15,7 @@ export default class App extends Component {
               <Route path="/contact-us" component={ContactUs} />
               <Route path="/sign-in" component={SignIn} />
               <Route path="/search-result" component={SearchResult} />
+              <Route path="/search-detail" component={SearchDetail} />
             </Switch>
         </Router>
       </div>
