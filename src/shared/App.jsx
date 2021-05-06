1e36bdf8cac825514de6f5a986c366e7472a1355
jennieyang95 <jennieyang95@gmail.com>
2021-05-05 22:39:16 -0700

    updated routing path to Profile

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home, WhoWeAre, ContactUs, SignIn, SearchResult, SearchDetail } from "../pages";
+import { Home, WhoWeAre, ContactUs, SignIn, Profile, SearchResult, SearchDetail } from "../pages";
 import Menu from "../components/Menu";
 
 export default class App extends Component {
@@ -14,6 +14,7 @@ export default class App extends Component {
               <Route path="/who-we-are" component={WhoWeAre} />
               <Route path="/contact-us" component={ContactUs} />
               <Route path="/sign-in" component={SignIn} />
+              <Route path="/profile" component={Profile} />
               <Route path="/search-result" component={SearchResult} />
               <Route path="/search-detail" component={SearchDetail} />
             </Switch>
