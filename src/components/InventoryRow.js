e7e7c55c94f4a0a848906aeacb79f339f7115913
Alex <hiennalex@gmail.com>
2021-05-07 14:40:06 -0700

    Inventory Page, currently working with dummy variables
    Search works, still figuring out header filter

---
+++ src/components/InventoryRow.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PencilFill } from "react-bootstrap-icons";
+import { Button } from "react-bootstrap";
+
+const InventoryRow = ({ carModel, carMake, carTrim, carPrice, Qty }) => {
+  return (
+    <tr>
+      <td>{carModel}</td>
+      <td>{carMake}</td>
+      <td>{carTrim}</td>
+      <td>{carPrice}</td>
+      <td>{Qty}</td>
+      <td className="lastColumn">
+        <Button className="editIcon" variant="light">
+          <PencilFill />
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default InventoryRow;
