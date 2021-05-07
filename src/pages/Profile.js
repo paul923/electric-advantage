d1b082f93922f68286bdad156b2542a6c98ba377
jennieyang95 <jennieyang95@gmail.com>
2021-05-07 11:51:21 -0700

    added change pw feature

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,48 +1,62 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/Home.css";
-import background from "../images/background.jpg";
 import { Button, Form, Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./css/Home.css";
+import SignUpModal from "./pageComponents/ProfilePopUp";
+import background from "../images/background.jpg";
 
 const Profile = () => {
-    return (
-        <body className="body">
-            <div className="TopImage">
-                <img className="d-block w-100" src={background} alt="background" className="background"/>
-            </div>
-            <div>
-                <div className="content">
-                    <h2 className="registrationText">Profile</h2>
+  const [registrationState, setRegistrationState] = useState("customer");
+  const [modalState, setModalState] = useState(false);
 
-                    <div className="leftColumn">
-                        <Form>
-                            <Form.Group controlId="Firstname">
-                                <Form.Control type="email" placeholder="First Name" />
-                                <Form.Text />
-                            </Form.Group>
-                            <Form.Group controlId="LastName" className="leftColumnChildren">
-                                <Form.Control type="email" placeholder="Last Name" />
-                                <Form.Text />
-                            </Form.Group>
-                            <Form.Group controlId="Email" className="leftColumnChildren">
-                                <Form.Control type="email" placeholder="Email" />
-                                <Form.Text />
-                            </Form.Group>
-                        </Form>
-                        <Button className="btn-success leftColumnChildren">Change Email/Password</Button>
+  return (
+    <body className="body">
+      <div className="TopImage">
+        <img className="d-block w-100" src={background} alt="background" className="background"/>
+      </div>
+      <div>
+        <div className="content">
+          <h2 className="registrationText">Profile</h2>
 
-                    
-                    </div>
-                </div>
-                </div>
+          <div className="login rightColumn">
+            <h2 className="registrationText"></h2>
+          </div>
+          <div className="leftColumn">
+            <Form>
+              <Form.Group controlId="Name">
+                <Form.Control type="email" placeholder="Name" />
+                <Form.Text />
+              </Form.Group>
+              <Form.Group controlId="Email" className="leftColumnChildren">
+                <Form.Control type="email" placeholder="Email" />
+                <Form.Text />
+              </Form.Group>
+            </Form>
 
-       
-        </body>
+          </div>
+          <div className="rightColumn">
+            
+            <Button
+              className="btn-success signUpButtons"
+              onClick={() => {
+                setModalState(true);
+                setRegistrationState("password");
+              }}
+            >
+              Change Password
+            </Button>
+            <SignUpModal
+              modalState={modalState}
+              setRegistrationState={setRegistrationState}
+              registrationState={registrationState}
+              setModalState={setModalState}
+            />
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
 
-
-
-
-    )
-}
-
-export default Profile
+export default Profile;
