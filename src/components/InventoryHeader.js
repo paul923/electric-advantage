30181f0460baab17c9e870eb9d036b058bc42f00
Alex <hiennalex@gmail.com>
2021-05-10 15:22:07 -0700

    Inventory list is searchable and sortable

--- src/components/InventoryHeader.js
+++ src/components/InventoryHeader.js
@@ -3,11 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import { ChevronExpand } from "react-bootstrap-icons";
 
-const InventoryHeader = ({ headerName }) => {
+const InventoryHeader = ({ headerName, sortHandler }) => {
   return (
     <th className="tableHeaders">
       {headerName}{" "}
-      <Button variant="light" className="headerButtons">
+      <Button variant="light" className="headerButtons" onClick={sortHandler}>
         <ChevronExpand />
       </Button>
     </th>
