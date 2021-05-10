4346411712d678add98be3c84f39dd47b640f7ac
Alex <hiennalex@gmail.com>
2021-05-09 20:18:48 -0700

    slider for price and range

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,82 +6,107 @@ import carImage1 from "../images/kia.jpg";
 import carImage2 from "../images/bmw.jpg";
 import carImage3 from "../images/jp.jpg";
 import carImage4 from "../images/ford.jpg";
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { Card, Table } from "react-bootstrap";
-
-
+import { Card, Table, Row, Col, Form } from "react-bootstrap";
+import RangeSlider from "react-bootstrap-range-slider";
 
 export default function LandingPage() {
-    const cardInfo = [
-        { image: carImage1, name: "Dt Kia", number: "50" },
-        { image: carImage2, name: "Brian Jessel Dt", number: "37" },
-        { image: carImage3, name: "Jim Pattison Dt", number: "2" },
-        { image: carImage4, name: "Ford Dt", number: "28" }
-    ]
+  const [priceValue, setPriceValue] = React.useState(125000);
+  const [rangeValue, setRangeValue] = React.useState(125000);
 
-    const renderCard = (card, index) => {
-        return (
-            <div className="dealerCard">
-                <img src={card.image} className="dealerImage" />
+  const cardInfo = [
+    { image: carImage1, name: "Dt Kia", number: "50" },
+    { image: carImage2, name: "Brian Jessel Dt", number: "37" },
+    { image: carImage3, name: "Jim Pattison Dt", number: "2" },
+    { image: carImage4, name: "Ford Dt", number: "28" },
+  ];
 
-                <Table striped hover className="dealerTable">
-                    <tbody>
-                        <tr>
-                            <td>{card.name}</td>
-                            <td>Inventory: {card.number}</td>
-
-                        </tr>
-
-
-                    </tbody>
-                </Table>
-
-
-            </div>
-        )
-    }
-
+  const renderCard = (card, index) => {
     return (
-        <body>
+      <div className="dealerCard">
+        <img src={card.image} className="dealerImage" />
 
-            <h1>15 Dealerships</h1>
-            <h1>215 New Cars</h1>
-            <h1>55 Used Cars</h1>
-
-
-
-
-            <h2>Top 4 Dealerships in your Region</h2>
-
-            <div className="dealerResults"> {cardInfo.map(renderCard)} </div>
-
-
-            <div className="makeColumn">
-                <h3>Make</h3>
-                <select className="make-dropdown">
-                    <option value="make1">make1</option>
-                    <option value="make2">make2</option>
-                    <option value="make3">make3</option>
-                </select>
-            </div>
-
-            <div className="statusColumn">
-                <h3>Status</h3>
-                <select className="status-dropdown">
-                    <option value="new">New</option>
-                    <option value="used">Used</option>
-                </select>
-            </div>
-
-
-
-     
-
-            
-            <Button className="searchButton">Search</Button>
-        </body>
-        
+        <Table striped hover className="dealerTable">
+          <tbody>
+            <tr>
+              <td>{card.name}</td>
+              <td>Inventory: {card.number}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     );
-}
+  };
 
+  return (
+    <body>
+      <h1>15 Dealerships</h1>
+      <h1>215 New Cars</h1>
+      <h1>55 Used Cars</h1>
+
+      <h2>Top 4 Dealerships in your Region</h2>
+
+      <div className="dealerResults"> {cardInfo.map(renderCard)} </div>
+
+      <Row>
+        <Col>
+          <div className="makeColumn">
+            <h3>Make</h3>
+            <select className="make-dropdown">
+              <option value="make1">make1</option>
+              <option value="make2">make2</option>
+              <option value="make3">make3</option>
+            </select>
+          </div>
+        </Col>
+
+        <Col>
+          <div className="statusColumn">
+            <h3>Status</h3>
+            <select className="status-dropdown">
+              <option value="new">New</option>
+              <option value="used">Used</option>
+            </select>
+          </div>
+        </Col>
+
+        <Col>
+          <section className="range">
+            <div className="priceColumn">
+              <h3>Price</h3>
+              <Container>
+                <RangeSlider
+                  max={300000}
+                  value={priceValue}
+                  onChange={(e) => setPriceValue(e.target.value)}
+                  variant="success"
+                />
+              </Container>
+            </div>
+            <h3>{priceValue}km</h3>
+          </section>
+        </Col>
+
+        <Col>
+          <section className="range">
+            <div className="rangeColumn">
+              <h3 className="">Range</h3>
+              <Container>
+                <RangeSlider
+                  max={300000}
+                  value={rangeValue}
+                  onChange={(e) => setRangeValue(e.target.value)}
+                  variant="success"
+                />
+              </Container>
+            </div>
+            <h3>${rangeValue}</h3>
+          </section>
+        </Col>
+      </Row>
+
+      <Button className="searchButton">Search</Button>
+    </body>
+  );
+}
