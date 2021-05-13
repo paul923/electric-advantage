cf9960641b8c6e7a4687b0bfce442a327b5e2be0
Alex <hiennalex@gmail.com>
2021-05-13 16:41:19 -0700

    updated make,mode.trim selection.

--- src/components/DealerAddCarModal.js
+++ src/components/DealerAddCarModal.js
@@ -1,6 +1,11 @@
 import { Modal, Form, Button, Row, Col } from "react-bootstrap";
 import "../pages/css/dealerAddCarModal.css";
 import React from "react";
+import {
+  getMakeList,
+  getModelListByMakeID,
+  getVehicleListByMakeIDAndModelID,
+} from "../api/VehicleAPI";
 
 const DealerAddCarModal = ({
   showModal,
@@ -17,6 +22,11 @@ const DealerAddCarModal = ({
   const [carID, setID] = React.useState("");
   const [carInfo, setInfo] = React.useState("");
   const [carImgs, setImgs] = React.useState("");
+  const [makeList, setMakeList] = React.useState([]);
+  const [selectedMake, setSelectedMake] = React.useState("1");
+  const [modelList, setModelList] = React.useState([]);
+  const [selectedModel, setSelectedModel] = React.useState("1");
+  const [trimList, setTrimList] = React.useState([]);
 
   const resetAllFieldsHandler = () => {
     setCarMake("");
@@ -31,6 +41,58 @@ const DealerAddCarModal = ({
     setModelDisabled(true);
     setTrimDisabled(true);
   };
+
+  React.useEffect(() => {
+    onLoadGetMakeList();
+    getModelList();
+  }, []);
+
+  React.useEffect(() => {
+    getModelList();
+  }, selectedMake);
+
+  React.useEffect(() => {
+    getTrimList();
+  }, selectedModel);
+
+  async function onLoadGetMakeList() {
+    let resultMakeList = await getMakeList();
+    let statusCode = resultMakeList.status;
+    if (statusCode === 200) {
+      let body = resultMakeList.body;
+      console.log(body);
+      setMakeList(body);
+    } else {
+      alert(`Status : ${statusCode}, ${resultMakeList.error}`);
+    }
+  }
+
+  async function getModelList() {
+    let resultModelList = await getModelListByMakeID(selectedMake);
+    let statusCode = resultModelList.status;
+    if (statusCode === 200) {
+      let body = resultModelList.body;
+      console.log(body);
+      setModelList(body);
+    } else {
+      alert(`Status : ${statusCode}, ${resultModelList.error}`);
+    }
+  }
+
+  async function getTrimList() {
+    let resultTrimList = await getVehicleListByMakeIDAndModelID(
+      selectedMake,
+      selectedModel
+    );
+    let statusCode = resultTrimList.status;
+    if (statusCode === 200) {
+      let body = resultTrimList.body;
+      console.log(body);
+      setTrimList(body);
+    } else {
+      alert(`Status : ${statusCode}, ${resultTrimList.error}`);
+    }
+  }
 
   const [modelDisabled, setModelDisabled] = React.useState(true);
   const [trimDisabled, setTrimDisabled] = React.useState(true);
@@ -62,40 +124,48 @@ const DealerAddCarModal = ({
           <Form.Group controlId="carMake">
             <Form.Control
               onChange={(e) => {
-                setCarMake(e.target.value);
+                let carMakeObject = JSON.parse(e.target.value);
+                setCarMake(carMakeObject.MakeName);
                 setModelDisabled(false);
+                setSelectedMake(carMakeObject.MakeID);
               }}
               as="select"
             >
               <option disabled selected>
                 Select Make...
               </option>
-              <option value="Hyundai">Hyundai</option>
-              <option value="Toyota">Toyota</option>
-              <option value="Porche">Porche</option>
-              <option value="Tesla">Tesla</option>
+              {makeList.map((carMake) => (
+                <option value={JSON.stringify(carMake)}>
+                  {carMake.MakeName}
+                </option>
+              ))}
             </Form.Control>
           </Form.Group>
-          <Form.Group
-            onChange={(e) => {
-              setCarModel(e.target.value);
-              setTrimDisabled(false);
-            }}
-            controlId="carModel"
-          >
-            <Form.Control as="select" disabled={modelDisabled}>
+          <Form.Group controlId="carModel">
+            <Form.Control
+              onChange={(e) => {
+                let carModelObject = JSON.parse(e.target.value);
+                setCarModel(carModelObject.ModelName);
+                setTrimDisabled(false);
+                setSelectedModel(carModelObject.ModelID);
+              }}
+              as="select"
+              disabled={modelDisabled}
+            >
               <option disabled selected>
                 Select Model...
               </option>
-              <option value="X">X</option>
-              <option value="Y">Y</option>
-              <option value="Z">Z</option>
-              <option value="M">M</option>
+              {modelList.map((carModel) => (
+                <option value={JSON.stringify(carModel)}>
+                  {carModel.ModelName}
+                </option>
+              ))}
             </Form.Control>
           </Form.Group>
           <Form.Group
             onChange={(e) => {
-              setCarTrim(e.target.value);
+              let carTrimObject = JSON.parse(e.target.value);
+              setCarTrim(carTrimObject.TrimName);
             }}
             controlId="carTrim"
           >
@@ -103,10 +173,11 @@ const DealerAddCarModal = ({
               <option disabled selected>
                 Select Trim...
               </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
+              {trimList.map((carTrim) => (
+                <option value={JSON.stringify(carTrim)}>
+                  {carTrim.TrimName}
+                </option>
+              ))}
             </Form.Control>
           </Form.Group>
         </Form>
@@ -125,6 +196,15 @@ const DealerAddCarModal = ({
               Price:{" "}
               <input
                 onChange={(e) => setCarPrice(e.target.value)}
+                className="columnInputs"
+              ></input>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              Year:{" "}
+              <input
+                onChange={(e) => setQty(e.target.value)}
                 className="columnInputs"
               ></input>
             </div>
