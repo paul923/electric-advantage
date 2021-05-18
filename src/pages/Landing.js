69fa17a49e9bfe168492424b503d0a7823fdd296
Alex <hiennalex@gmail.com>
2021-05-17 17:22:47 -0700

    adding functionality to landing/search results

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/landing.css";
 
+import {
+  getMakeList,
+  getModelListByMakeID,
+  getVehicleListByMakeIDAndModelID,
+  getVehicleSearchResult,
+} from "../api/VehicleAPI";
 import carImage1 from "../images/kia.jpg";
 import carImage2 from "../images/bmw.jpg";
 import carImage3 from "../images/jp.jpg";
@@ -12,8 +18,14 @@ import { Card, Table, Row, Col, Form } from "react-bootstrap";
 import RangeSlider from "react-bootstrap-range-slider";
 
 export default function LandingPage() {
+  const [makeList, setMakeList] = React.useState([]);
   const [priceValue, setPriceValue] = React.useState(125000);
   const [rangeValue, setRangeValue] = React.useState(500);
+  const [makeID, setMakeID] = React.useState(1);
+  const [conditionID, setConditionID] = React.useState(1);
+  const [lat, setLat] = React.useState("");
+  const [long, setLong] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
 
   const cardInfo = [
     { image: carImage1, name: "Downtown Kia", number: "50" },
@@ -39,13 +51,66 @@ export default function LandingPage() {
     );
   };
 
+  React.useEffect(() => {
+    onLoadGetMakeList();
+    onLoadCheckGeolocation();
+  }, []);
+
+  function success(pos) {
+    setLat(pos.coords.latitude);
+    setLong(pos.coords.longitude);
+    console.log(
+      `Longitude : ${pos.coords.longitude}\nLatitude : ${pos.coords.latitude}`
+    );
+  }
+
+  function error(err) {
+    alert("Turn on geolocation!");
+  }
+
+  function onLoadCheckGeolocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      alert("Must turn on Geolocation!");
+    }
+  }
+
+  async function onClickgetVehicleSearchResult() {
+    let resultSearch = await getVehicleSearchResult(
+      makeID,
+      rangeValue,
+      priceValue,
+      conditionID,
+      lat,
+      long
+    );
+    console.log(resultSearch);
+  }
+
+  async function onLoadGetMakeList() {
+    let resultMakeList = await getMakeList();
+    let statusCode = resultMakeList.status;
+    if (statusCode === 200) {
+      let body = resultMakeList.body;
+      console.log(body);
+      setMakeList(body);
+    } else {
+      alert(`Status : ${statusCode}, ${resultMakeList.error}`);
+    }
+  }
+
   return (
     <body>
-        <div className = "box1"><h1>15 Dealerships</h1></div>
-        <div className = "box2"><h1>215 New Cars</h1></div>
-        <div className = "box3"><h1>55 Used Cars</h1></div>
-      
-
+      <div className="box1">
+        <h1>15 Dealerships</h1>
+      </div>
+      <div className="box2">
+        <h1>215 New Cars</h1>
+      </div>
+      <div className="box3">
+        <h1>55 Used Cars</h1>
+      </div>
 
       <h2>Top 4 Dealerships in your Region</h2>
 
@@ -54,27 +119,33 @@ export default function LandingPage() {
       <Row>
         <Col>
           <div className="makeColumn">
-          <div className="makeTitle">
-            <h3>Make</h3>
+            <div className="makeTitle">
+              <h3>Make</h3>
             </div>
 
-            <select className="make-dropdown">
-              <option value="make1">make1</option>
-              <option value="make2">make2</option>
-              <option value="make3">make3</option>
+            <select
+              className="make-dropdown"
+              onChange={(e) => setMakeID(e.target.value)}
+            >
+              {makeList.map((make) => (
+                <option value={make.MakeID}>{make.MakeName}</option>
+              ))}
             </select>
           </div>
         </Col>
 
         <Col>
           <div className="statusColumn">
-          <div className="statusTitle">
-            <h3>Status</h3>
+            <div className="statusTitle">
+              <h3>Status</h3>
             </div>
 
-            <select className="status-dropdown">
-              <option value="new">New</option>
-              <option value="used">Used</option>
+            <select
+              className="status-dropdown"
+              onChange={(e) => setConditionID(e.target.value)}
+            >
+              <option value={parseInt("1", 10)}>New</option>
+              <option value={parseInt("2", 10)}>Used</option>
             </select>
           </div>
         </Col>
@@ -82,8 +153,8 @@ export default function LandingPage() {
         <Col>
           <section className="range">
             <div className="priceColumn">
-            <div className="priceTitle">
-              <h3>Price</h3>
+              <div className="priceTitle">
+                <h3>Price</h3>
               </div>
 
               <Container>
@@ -95,15 +166,14 @@ export default function LandingPage() {
                 />
               </Container>
             </div>
-            
           </section>
         </Col>
 
         <Col>
           <section className="range">
             <div className="rangeColumn">
-            <div className="rangeTitle">
-              <h3 className="">Range</h3>
+              <div className="rangeTitle">
+                <h3 className="">Range</h3>
               </div>
 
               <Container>
@@ -115,19 +185,20 @@ export default function LandingPage() {
                 />
               </Container>
             </div>
-            
           </section>
         </Col>
 
         <Col>
-          <Button className="searchButton">
-            <NavLink to="/search-result" className="nav-search">Search</NavLink>
-          </Button>
+          <NavLink to="/search-result" className="nav-search">
+            <Button
+              className="searchButton"
+              onClick={() => setSearchResults(onClickgetVehicleSearchResult())}
+            >
+              Search
+            </Button>
+          </NavLink>
         </Col>
-
       </Row>
-
-
     </body>
   );
 }
