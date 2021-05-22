264939413bbb5d4851d36248aaea326e552a0e35
Alex <hiennalex@gmail.com>
2021-05-21 21:10:46 -0700

    basics of dealer inventory seems to work... still need to fix bugs and add features

--- src/pages/dealerPages/Inventory.js
+++ src/pages/dealerPages/Inventory.js
@@ -6,7 +6,6 @@ import { ChevronExpand } from "react-bootstrap-icons";
 import InventoryRow from "../../components/DealerInventoryRow";
 import InventoryHeader from "../../components/DealerInventoryHeader";
 import { Link } from "react-router-dom";
-import DealerEditCarModal from "../../components/DealerEditModal";
 import {
   getAllDealerships,
   getInventoryByDealershipID,
@@ -14,152 +13,45 @@ import {
 } from "../../api/DealershipAPI";
 
 export default function DealerInventory() {
-  /* DUMMY DATA WITH DUMMY FIELDS */
-
-  // TESTING API
-  // async function getFirstDealerID() {
-  //   let firstID = await getAllDealerships();
-  //   console.log("FIRST DEALERSHIP ID:" + firstID.body[0]["DealershipID"]);
-  // }
-  // getFirstDealerID();
-  // /* "CHEV001" */
-
-  let car1 = {
-    carID: 1,
-    carMake: "Toyota",
-    carModel: "Corolla",
-    carTrim: "b",
-    carYear: 2021,
-    carColor: "black",
-    carPrice: 999,
-    Qty: 99,
-  };
-  let car2 = {
-    carID: 2,
-    carMake: "Honda",
-    carModel: "Civic",
-    carTrim: "A",
-    carYear: 2019,
-    carColor: "silver",
-    carPrice: 99,
-    Qty: 10,
-  };
-  let car3 = {
-    carID: 3,
-    carMake: "Mercedes",
-    carModel: "CLS",
-    carTrim: "C",
-    carYear: 2001,
-    carColor: "white",
-    carPrice: 50,
-    Qty: 89,
-  };
-  let car4 = {
-    carID: 4,
-    carMake: "Volkswagen",
-    carModel: "Jetta",
-    carTrim: "Q",
-    carYear: 2018,
-    carColor: "grey",
-    carPrice: 150,
-    Qty: 2,
-  };
-  let car5 = {
-    carID: 5,
-    carMake: "BMW",
-    carModel: "X1",
-    carTrim: "z",
-    carYear: 2021,
-    carColor: "black",
-    carPrice: 1,
-    Qty: 28,
-  };
-  let car6 = {
-    carID: 6,
-    carMake: "Porsche",
-    carModel: "Taycan",
-    carTrim: "abcd",
-    carYear: 2011,
-    carColor: "blue",
-    carPrice: 999,
-    Qty: 0,
-  };
-  let car7 = {
-    carID: 7,
-    carMake: "Mercedes",
-    carModel: "CLA",
-    carTrim: "M",
-    carYear: 2022,
-    carColor: "white",
-    carPrice: 99,
-    Qty: 16,
-  };
-  let car8 = {
-    carID: 8,
-    carMake: "Toyota",
-    carModel: "Corolla",
-    carTrim: "P",
-    carYear: 2002,
-    carColor: "red",
-    carPrice: 100,
-    Qty: 36,
-  };
-  let car9 = {
-    carID: 9,
-    carMake: "BMW",
-    carModel: "X3",
-    carTrim: "O",
-    carYear: 2015,
-    carColor: "grey",
-    carPrice: 342,
-    Qty: 43,
-  };
-
-  // let listOfCars = [car1, car2, car3, car4, car5, car6, car7, car8, car9];
-  /* END OF DUMMY DATA */
-
-  // let listOfCars = [car1, car2, car3, car4, car5, car6, car7, car8, car9];
-
-  const [listOfCars, setListOfCars] = React.useState([
-    car1,
-    car2,
-    car3,
-    car4,
-    car5,
-    car6,
-    car7,
-    car8,
-    car9,
-  ]);
-
   const [retrievedInventory, setRetrievedInventory] = React.useState([]);
-  const [inventory, setInventory] = React.useState(listOfCars);
+
+  async function getFirstInventoryList() {
+    let firstInventory = await getInventoryByDealershipID("1");
+    console.log("HERE");
+    let statusCode = firstInventory.status;
+    if (statusCode === 200) {
+      let body = firstInventory.body;
+      setRetrievedInventory(
+        body.map((car) => {
+          return {
+            carModel: car.ModelName,
+            carTrim: car.Odometer,
+            carMake: car.MakeName,
+            carYear: car.Year,
+            carColor: car.ColorID,
+            carQty: car.Quantity,
+            carPrice: car.StartPrice,
+          };
+        })
+      );
+    } else {
+      alert(`Status : ${statusCode}}`);
+    }
+  }
+
+  React.useEffect(() => {
+    getFirstInventoryList();
+  }, []);
+
+  React.useEffect(() => {
+    setFilteredList(retrievedInventory);
+  }, [retrievedInventory]);
+
   const [filteredList, setFilteredList] = React.useState([]);
   const [query, setQuery] = React.useState("");
   const [reverse, setReverse] = React.useState(false);
   const [showModal, setShowModal] = React.useState(false);
   const [editText, showEditText] = React.useState(true);
-
-  // setAddList(
-  //   carsToAdd.map((car) => {
-  //     return {
-  //       VehicleID: car.vehicleID,
-  //       DealershipID: "1",
-  //       ColorID: car.carColor,
-  //       ConditionID: parseInt(car.carCondition),
-  //       StartPrice: parseFloat(car.carPrice),
-  //       Odometer: parseFloat(car.Odo),
-  //       Quantity: parseInt(car.Qty),
-  //     };
-  //   })
-  // );
-
-  async function getFirstInventoryList() {
-    let firstInventory = await getInventoryByDealershipID("1");
-    console.log("FIRST DEALERSHIP INVENTORY:");
-    console.log(firstInventory);
-  }
-  getFirstInventoryList();
 
   function sortNumber(sortBy) {
     const copy = [...filteredList];
@@ -197,23 +89,21 @@ export default function DealerInventory() {
     setFilteredList(copy);
   }
 
-  function search(listOfCars) {
-    return listOfCars.filter(
+  function search(retrievedInventory) {
+    return retrievedInventory.filter(
       (car) =>
         car.carModel.toString().toLowerCase().indexOf(query.toLowerCase()) >
           -1 ||
         car.carMake.toString().toLowerCase().indexOf(query.toLowerCase()) >
           -1 ||
-        car.carTrim.toString().toLowerCase().indexOf(query.toLowerCase()) >
-          -1 ||
         car.carPrice.toString().toLowerCase().indexOf(query.toLowerCase()) >
           -1 ||
-        car.Qty.toString().toLowerCase().indexOf(query.toLowerCase()) > -1
+        car.carQty.toString().toLowerCase().indexOf(query.toLowerCase()) > -1
     );
   }
 
   React.useEffect(() => {
-    setFilteredList(search(inventory));
+    setFilteredList(search(retrievedInventory));
   }, [query]);
 
   return (
@@ -263,7 +153,6 @@ export default function DealerInventory() {
                   <ChevronExpand />
                 </Button>
               </th>
-              {/* <th className="tableHeaders"> </th> */}
             </tr>
           </thead>
           <tbody className="body">
@@ -271,12 +160,12 @@ export default function DealerInventory() {
               <InventoryRow
                 carModel={car.carModel}
                 carMake={car.carMake}
-                carTrim={car.carTrim}
+                carTrim={car.carYear}
                 carYear={car.carYear}
                 carColor={car.carColor}
                 carPrice={car.carPrice}
-                Qty={car.Qty}
-                carID={car.carID}
+                Qty={car.carQty}
+                // carID={car.carID}
                 row={car}
                 setShowModal={setShowModal}
                 editText={editText}
