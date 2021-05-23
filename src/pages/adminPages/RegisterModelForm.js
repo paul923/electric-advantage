b283d9bd4bd51db5c1570b60f831468888d2bc9f
jennieyang95 <jennieyang95@gmail.com>
2021-05-23 15:28:35 -0700

    updated popup, need to fix bugs

--- src/pages/adminPages/RegisterModelForm.js
+++ src/pages/adminPages/RegisterModelForm.js
@@ -5,12 +5,26 @@ import { useForm, Form } from '../../components/AdminUseForm';
 import * as vehicleService from "./vehicleService";
 
 import {
-    getMakeList
+    getMakeList,
+    registerModelWithMakeID
   } from "../../api/VehicleAPI";
 import { Select, MenuItem } from "@material-ui/core";
 
+const initialFValues = {
+    id: 0,
+    ModelID: '',  
+    MakeName: '',
+    MakeID: ''
+}
+
 
 export default function RegisterModelForm(props) {
+    const { addOrEdit, recordForEdit } = props
+    const [id, setID] = React.useState("");
+    const [makeName, setMakeName] = React.useState("");
+    const [modelID, setModelID] = React.useState("");
+    const [makeID, setMakeID] = React.useState("");
+
     const [makeOpen, setMakeOpen] = React.useState(false);
     const [makeList, setMakeList] = React.useState("");
     const [selectedMakeID, setSelectedMakeID] = React.useState("");
@@ -20,6 +34,15 @@ export default function RegisterModelForm(props) {
 
     }, []);
   
+
+    async function onClickRegisterModelWithMakeID() {
+        let modelObj = {
+            ModelID: id,
+            MakeName: makeName,
+        };
+        let result = await registerModelWithMakeID(modelObj);
+        alert(`Status : ${result.status}, ${result.body}`);
+    }
   
     async function onLoadGetMakeList() {
       let resultMakeList = await getMakeList();
@@ -32,6 +55,43 @@ export default function RegisterModelForm(props) {
         alert(`Status : ${statusCode}, ${resultMakeList.error}`);
       }
     }
+
+
+    const validate = (fieldValues = values) => {
+        let temp = { ...errors }
+        if ('MakeID' in fieldValues)
+            temp.carID = fieldValues.carID ? "" : "This field is required."
+    
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+    }
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    } = useForm(initialFValues, true, validate);
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (validate()) {
+            addOrEdit(values, resetForm);
+        }
+    }
+
+    useEffect(() => {
+        if (recordForEdit != null)
+            setValues({
+                ...recordForEdit
+            })
+    }, [recordForEdit])
   
   
     const vehiclesList = () => {
@@ -54,6 +114,37 @@ export default function RegisterModelForm(props) {
                   </MenuItem>
                 );
               })}
+
+<Form onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <Controls.Input
+                        label="Model ID"
+                        value={id}
+                        onChange={(event) => setID(event.target.value)}
+                    />
+                    <Controls.Input
+                        label="Make Name"
+                        value={makeName}
+                        onChange={(event) => setMakeName(event.target.value)}
+                    />
+                   
+                </Grid>
+                <Grid item xs={6}>
+                    <div>
+                        <Controls.Button
+                            type="submit"
+                            text="Submit"
+                            onClick= {() => 
+                                onClickRegisterModelWithMakeID()} />
+                        <Controls.Button
+                            text="Reset"
+                            color="default"
+                            onClick={resetForm} />
+                    </div>
+                </Grid>
+            </Grid>
+        </Form>
           </Select>
         </div>
       );
